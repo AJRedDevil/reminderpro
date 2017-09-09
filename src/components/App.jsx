d012daa606b29e0dc897d07dd997a093f5ccc866
AJRedDevil <ajan.shresh@gmail.com>
2017-09-09 09:20:13 +0545

    Deleting a reminder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import { addReminder } from '../actions';
+import { addReminder, deleteReminder } from '../actions';
 // import { bindActionCreators } from 'redux';
 
 class App extends Component {
@@ -10,6 +10,11 @@ class App extends Component {
 
     addReminder = () => {
         this.props.addReminder(this.state.text);
+    }
+
+    deleteReminder = (id) => {
+        console.log('deleting in application', id);
+        console.log('this.props', this.props);
     }
 
     renderReminders = () => {
@@ -23,7 +28,12 @@ class App extends Component {
                                 key={reminder.id}
                                 className="list-group-item"
                             >
-                                <div>{reminder.text}</div>
+                                <div className="list-item">{reminder.text}</div>
+                                <div
+                                    className="list-item delete-button"
+                                    onClick={() => this.deleteReminder(reminder.id)}>
+                                    &#x2715;
+                                </div>
                             </li>
                         )
                     })
@@ -73,4 +83,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps, { addReminder })(App);
+export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
